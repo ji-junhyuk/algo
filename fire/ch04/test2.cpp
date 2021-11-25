4f3857899c7585b429a568580b5db1f1209f88eb
ji-junhyuk <takeoutmoneywallet@naver.com>
2021-11-25 18:32:47 +0900

    run

---
+++ fire/ch04/test2.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	char str1[10], str2[10];
+	printf("str1: ");
+	scanf("%s", str1);
+	printf("입력한 str1: %s\n", str1);
+
+	printf("str2: ");
+	scanf("%s", str2);
+	printf("입력한 str2: %s\n", str2);
+	return 0;
+}
