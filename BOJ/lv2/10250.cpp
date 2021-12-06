8ed7d2b4342f04a3cb82114f416772a908ad2a17
junto <takeoutmoneywallet@naver.com>
2021-12-07 01:55:26 +0900

    run

--- BOJ/lv2/10250.cpp
+++ BOJ/lv2/10250.cpp
@@ -8,34 +8,17 @@ int main()
 
 	int T;
 	int H, W, N;
+	int result;
+
 	cin >> T;
 	while (T--)
 	{
 		cin >> H >> W >> N;
-		/*
-		   층수결정: 손님(N)을 층수(H)로 나누었을 때 몫
-		   1) 손님이 층수랑 같을 때 -> 층수는 손님
-		   2) 손님이 층수보다 작을 때 -> 
-		   3) 손님이 층수보다 클 때 -> 층수는 몫
-		   호수결정: 손님(N)을 층수(H)로 나누었을 때 몫
-		*/
-		if (N < H)
-			cout << 1;
-		else if (N == H)
-			cout << N;
-		else
-		{
-			if (N % H == 0)
-				cout << H;
-			else
-				cout << N % H;
-		}
-		// 호수 결정
-		if (N / H < 10)
-			cout << 0;
+
 		if (N % H == 0)
-			cout << N / H << '\n';
+			result = H * 100 + (N / H);
 		else
-			cout << N / H + 1 << '\n';
+			result = N % H * 100 + (N / H) + 1;
+		cout << result << '\n';
 	}
 }
