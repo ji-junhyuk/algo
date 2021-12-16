54ad6369e4472d674b160ebbf18f0a494dfa7160
junto <takeoutmoneywallet@naver.com>
2021-12-16 22:13:42 +0900

    run

---
+++ BOJ/lv2/11650.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+    
+	cin >> N;
+    vector<pair<int, int>> v(N);
+   
+   	for (int i = 0; i < N; i++)
+        cin >> v[i].first >> v[i].second;
+    sort(v.begin(), v.end());
+    for (int i = 0; i < N; i++)
+        cout << v[i].first << ' ' << v[i].second<< '\n';
+}
