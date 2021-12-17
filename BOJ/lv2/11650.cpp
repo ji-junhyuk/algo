47f82243fc05073097295ea4110167a16cadb799
junto <takeoutmoneywallet@naver.com>
2021-12-17 23:58:30 +0900

    run

--- BOJ/lv2/11650.cpp
+++ BOJ/lv2/11650.cpp
@@ -1,6 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+bool cmp(pair<int, int> a, pair<int, int> b)
+{
+	if (a.second < b.second)
+	   	return true;
+	else if (a.second == b.second)
+		if (a.first < b.first)
+			return true;
+	return false;
+}
+
 int main()
 {
 	ios::sync_with_stdio(0);
@@ -13,7 +23,7 @@ int main()
    
    	for (int i = 0; i < N; i++)
         cin >> v[i].first >> v[i].second;
-    sort(v.begin(), v.end());
+    sort(v.begin(), v.end(), cmp);
     for (int i = 0; i < N; i++)
         cout << v[i].first << ' ' << v[i].second<< '\n';
 }
