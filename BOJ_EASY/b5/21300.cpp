d3e6eebb8bb4a123c20ddce2128e24f18ab1e7ae
ji junhyuk <jijunhyuk@jis-MacBook-Pro.local>
2022-05-14 10:13:56 +0900

    r

---
+++ BOJ_EASY/b5/21300.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int a, b, c, d, e, f;
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> a >> b >> c >> d >> e >> f;
+	cout << ((5 * a) + (5 * b) + (5 * c) + (5 * d) + (5 * e) + (5 * f));
+}
---
+++ BOJ_EASY/b5/22193.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int	a, b, c, d;
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> a >> b >> c >> d;
+	cout << c * d;
+}
