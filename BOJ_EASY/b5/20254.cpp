3b920ecbe97e978bc528f2447bbd418f30c3ac35
ji junhyuk <jijunhyuk@192.168.nate.com>
2022-05-17 10:48:24 +0900

    r

---
+++ BOJ_EASY/b5/20254.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b, c ,d;
+
+	cin >> a >> b >> c >> d;
+
+	cout << (a * 56) + (b * 24) + (c * 14) + (d * 6);
+}
