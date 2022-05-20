2816c0522f406de3dc2a55e2dcc1f6663b74d898
ji junhyuk <jijunhyuk@jis-MacBook-Pro.local>
2022-05-21 08:02:49 +0900

    r

---
+++ BOJ_EASY/b5/15894.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// 1 -> 4
+// 2 -> 8
+// 3 -> 12
+long long	n;
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cin >> n;
+	cout << 4 * n;
+}
